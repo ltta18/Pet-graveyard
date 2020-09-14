29226023b00b9fa2f410421492f6a70950e68f5f
Tran Trong Hung <tronghung199@gmail.com>
2020-09-14 20:27:12 +0700

    add layout UI for cemetery page

--- src/components/GraveyardZone/Headstone.js
+++ src/components/GraveyardZone/Headstone.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import './Headstone.css';
-import { CardMedia, Card, CardActionArea, Typography, CardContent } from '@material-ui/core';
+import React from "react";
+import "./Headstone.css";
+import {
+  CardMedia,
+  Card,
+  CardActionArea,
+  Typography,
+  CardContent,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 function Headstone(props) {
-  const { img, name } = props
+  const { img, name } = props;
 
   return (
-    <Card className="headstone">
-      <CardActionArea>
-        <CardMedia 
-          component="img"
-          alt="Headstone Image"
-          height="10%"
-          image={img}
-          title="Headstone Image"
-        />
-        <CardContent>
-        <Typography component="p">{name}</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link to={{ pathname: `cemetery/${name}` }}>
+      <Card className="headstone">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Headstone Image"
+            height="10%"
+            image={img}
+            title="Headstone Image"
+          />
+          <CardContent>
+            <Typography component="p">{name}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
 
-export default Headstone
+export default Headstone;
--- src/pages/cemetery/index.js
+++ src/pages/cemetery/index.js
@@ -1,9 +1,105 @@
 import React from "react";
+import "./index.css";
+import {
+  Button,
+  Container,
+  Grid,
+  InputBase,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import Slider from "react-slick";
+const Cemetery = (props) => {
+  const { id } = props.match.params;
 
-import { Container } from "@material-ui/core";
+  const settings = {
+    // customPaging: function (i) {
+    //   return (
+    //     <a>
+    //       <img src={`${baseUrl}/abstract0${i + 1}.jpg`} />
+    //     </a>
+    //   );
+    // },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Container className="cemetery-container" maxWidth="lg">
+      <Grid container justify="center" alignItems="center">
+        <Grid className="cemetery-title">
+          <Typography variant="h4" component="h2">
+            {id}
+          </Typography>
+        </Grid>
 
-const Cemetery = () => {
-  return <Container>Cemetery</Container>;
+        <Grid className="cemetery-content" container spacing={2}>
+          <Grid item xs={5}>
+            <Slider {...settings}>
+              <div>
+                <img
+                  className="img"
+                  src="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
+                />
+              </div>
+              <div>
+                <img
+                  className="img"
+                  src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1144982182.jpg"
+                />
+              </div>
+              <div>
+                <img
+                  className="img"
+                  src="https://images.english.elpais.com/resizer/8cNVrZOU1KaW0whQODrMSxdaofY=/768x0/filters:focal(1052x792:1062x802)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/IPZM424KYBEH7IVUKNQZETWHVU.jpg"
+                />
+              </div>
+              <div>
+                <img
+                  className="img"
+                  src="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
+                />
+              </div>
+              <div>
+                <img
+                  className="img"
+                  src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1144982182.jpg"
+                />
+              </div>
+              <div>
+                <img
+                  className="img"
+                  src="https://images.english.elpais.com/resizer/8cNVrZOU1KaW0whQODrMSxdaofY=/768x0/filters:focal(1052x792:1062x802)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/IPZM424KYBEH7IVUKNQZETWHVU.jpg"
+                />
+              </div>
+            </Slider>
+          </Grid>
+          <Grid className="cemetery-pet" item xs={7}>
+            <Grid className="cemetery-pet-info">
+              <Typography align="center" variant="h5" component="h2">
+                Giới thiệu chung
+              </Typography>
+            </Grid>
+            <Grid container className="pet-info-comment" spacing={2}>
+              <Grid className="comment-box" item xs={8}>
+                <InputBase
+                  className="comment-input"
+                  fullWidth
+                  placeholder="Hãy gõ lời bạn muốn nhắn nhủ ở đây"
+                />
+              </Grid>
+              <Grid className="comment-send" item xs={4}>
+                <Button variant="contained">Thắp hương</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Cemetery;
