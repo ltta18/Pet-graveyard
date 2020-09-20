416297f4ea554ee25adc0fcba02ce4437e562f04
ltta18 <thuylinh7720@gmail.com>
2020-09-21 00:58:47 +0700

    Add Modal

--- src/components/graveyardzone/GraveyardZone.js
+++ src/components/graveyardzone/GraveyardZone.js
@@ -1,25 +1,32 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, Modal, Typography } from "@material-ui/core";
 import React from "react";
 import "./GraveyardZone.css";
 import Headstone from "./Headstone";
 
 function GraveyardZone(props) {
-  const { zoneName, handleClose } = props;
+  const { zoneName, handleClose, open } = props;
 
   const indexArr = [...Array(30).keys()];
 
   return (
-    <Container id="graveyard-zone" style={{ display: "none" }}>
-      <Container className="graveyard-body">
-        <Container className="header">
-          <Typography className="zone-name">{zoneName}</Typography>
-          <button className="x-icon" onClick={handleClose}></button>
-        </Container>
-        <Container className="headstone-list">
-          {indexArr.map(index => <Headstone name={`${zoneName}${index+1}`} img={require("../../img/book.jpg")} key={index}/>)}
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+      <Container id="graveyard-zone" maxWidth={false}>
+        <Container className="graveyard-body">
+          <Container className="header">
+            <Typography className="zone-name">{zoneName}</Typography>
+            <button className="x-icon" onClick={handleClose}></button>
+          </Container>
+          <Container className="headstone-list">
+            {indexArr.map(index => <Headstone name={`${zoneName}${index+1}`} img={require("../../img/book.jpg")} key={index}/>)}
+          </Container>
         </Container>
       </Container>
-    </Container>
+    </Modal>
   );
 }
 
