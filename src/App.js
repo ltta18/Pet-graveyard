7b2f65cb9e6288c4ab46cea629058436d6cc21b1
Tran Trong Hung <tronghung199@gmail.com>
2020-10-15 12:41:18 +0700

    update style font & favicon

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import history from "./utils/history";
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({  
+  typography: {
+    fontFamily: 
+    '"Barlow", -apple-system, Roboto, sans-serif',
+  },
+  h4: {
+      fontWeight: 700,
+    },
+});
 
 function App() {
   return (
     <React.StrictMode>
-      <BrowserRouter history={history}>
-        <Layout />
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter history={history}>
+          <Layout />
+        </BrowserRouter>
+      </MuiThemeProvider>
+      
     </React.StrictMode>
   );
 }
