3abc71fe911d0bc8ca662b7f2645ec4823de73d1
ltta18 <thuylinh7720@gmail.com>
2020-10-28 16:39:30 +0700

    Responsive TopNav

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from "react-router-dom";
 import "./Header.css";
 
-function Header() {
+function Header() {    
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const width = window.innerWidth;
+    console.log(width)
+    width < 870 ? setMobile(true) : setMobile(false)
+  }, [])
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Grid id="header">
       <h1>
@@ -12,14 +33,39 @@ function Header() {
       </h1>
 
       {/* <img src="" alt="logo" id="logo" /> */}
-      <Grid>
+      {!mobile && 
+      <div>
         <Button className="button">Donate</Button>
         <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSccrmh_nMr2Clpm6JlpgISpucRkunI4hNGKCNjrwILF9CTfLw/viewform">
           <Button className="button">
                   Đăng kí mộ
           </Button>
         </a>
-      </Grid>
+      </div>
+      }
+      {mobile &&
+      <div>
+        <IconButton edge="start" aria-label="menu" onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Donate</MenuItem>
+          <MenuItem onClick={handleClose}>
+          <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSccrmh_nMr2Clpm6JlpgISpucRkunI4hNGKCNjrwILF9CTfLw/viewform">
+          
+                  Đăng kí mộ
+          
+        </a>
+          </MenuItem>
+        </Menu>
+    </div>
+    }
     </Grid>
   );
 }
