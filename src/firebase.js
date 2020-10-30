f934e4148eda8d7c83b896999c9775e7ed4a7327
Tran Trong Hung <tronghung199@gmail.com>
2020-10-30 11:11:56 +0700

    update key

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 
+export const FIREBASE_APIKEY = process.env.REACT_APP_FIREBASE_APIKEY;
+
+
 var firebaseConfig = {
-    apiKey: "AIzaSyA_j6l3XzrlVK-s-KFonCu52pOwZDbM5EM",
+    apiKey: FIREBASE_APIKEY,
     authDomain: "pet-graveyard.firebaseapp.com",
     databaseURL: "https://pet-graveyard.firebaseio.com",
     projectId: "pet-graveyard",
     storageBucket: "pet-graveyard.appspot.com",
     messagingSenderId: "129486054186",
     appId: "1:129486054186:web:6b098fdabd6961401e3453",
-    measurementId: "G-PQHVFV9862"};
+    measurementId: "G-PQHVFV9862"
+};
 
 firebase.initializeApp(firebaseConfig)
 
